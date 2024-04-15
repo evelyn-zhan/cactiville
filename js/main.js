5f212bd26a0d83bbff040afcf48debc1ea49e232
Evelyn <evelynzanpx@gmail.com>
2024-04-15 10:47:32 +0700

    New Section

--- js/main.js
+++ js/main.js
@@ -27,3 +27,12 @@ const linkAction = () => {
 };
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/* Change Header Background */
+const blurHeader = () => {
+    const header = document.getElementById('header');
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header');
+}
+
+window.addEventListener('scroll', blurHeader);
